60c83293d1ef2008b0924d950cb7f50b17a2dc43
Papp Balázs <code.balazs.papp@gmail.com>
2021-02-23 18:35:58 +0100

    Added maths.

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hazel/vendor/imgui"
+IncludeDir["glm"] = "Hazel/vendor/glm"
 
 group "Dependencies"
 	include "Hazel/vendor/GLFW"
@@ -47,7 +48,8 @@ project "Hazel"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -107,7 +109,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Hazel/src",
-		"Hazel/vendor/spdlog/include"
+		"Hazel/vendor/spdlog/include",
+		"%{IncludeDir.glm}"
 	}
 
 	links
